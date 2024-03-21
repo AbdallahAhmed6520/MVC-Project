1e93ceb01f7da6461a3823a08e522cf40bb497ce
abdosedroho <abdosedroho@gmail.com>
2024-03-21 08:23:08 +0200

    Add Interface and Repository Folders and make Dependency Injection For DB

---
+++ Demo.BLL/Interfaces/IDepartmentRepository.cs
@@ -0,0 +1,18 @@
+﻿using Demo.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.BLL.Interfaces
+{
+	public interface IDepartmentRepository
+	{
+		public IEnumerable<Department> GetAll();
+		public Department GetById(int id);
+		public int Add(Department department);
+		public int Update(Department department);
+		public int Delete(int id);
+	}
+}
---
+++ Demo.DAL/Contexts/MVCAppContext.cs
@@ -0,0 +1,22 @@
+﻿using Demo.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.DAL.Contexts
+{
+	public class MVCAppContext : DbContext
+	{
+		public MVCAppContext(DbContextOptions<MVCAppContext> options) : base(options)
+		{
+
+		}
+		//protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		//=> optionsBuilder.UseSqlServer("Server = .; Database = MVCApp; Trusted_Connection = true;");
+
+		public DbSet<Department> Departments { get; set; }
+	}
+}
