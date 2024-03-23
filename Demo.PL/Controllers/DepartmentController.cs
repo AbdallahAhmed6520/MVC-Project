918e9fef474547bc2ac77f761e18a8f34d7368a2
abdosedroho <abdosedroho@gmail.com>
2024-03-23 05:26:44 +0200

    Add Department Index Action And Basic View For It

--- Demo.PL/Controllers/DepartmentController.cs
+++ Demo.PL/Controllers/DepartmentController.cs
@@ -14,7 +14,8 @@ namespace Demo.PL.Controllers
 		}
 		public IActionResult Index()
 		{
-			return View();
+			var departments = _departmentRepository.GetAll();
+			return View(departments);
 		}
 	}
 }
