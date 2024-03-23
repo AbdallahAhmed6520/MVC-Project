9d5888864b083318e92c5a4bef56269726d77a6d
abdosedroho <abdosedroho@gmail.com>
2024-03-24 00:49:59 +0200

    Add Employee Interface And Repository

---
+++ Demo.BLL/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,18 @@
+﻿using Demo.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.BLL.Interfaces
+{
+    public interface IEmployeeRepository
+    {
+        public IEnumerable<Employee> GetAll();
+        public Employee GetById(int id);
+        public int Add(Employee employee);
+        public int Update(Employee employee);
+        public int Delete(Employee employee);
+    }
+}
---
+++ Demo.BLL/Repositories/EmployeeRepository.cs
@@ -0,0 +1,46 @@
+﻿using Demo.BLL.Interfaces;
+using Demo.DAL.Contexts;
+using Demo.DAL.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Demo.BLL.Repositories
+{
+    public class EmployeeRepository:IEmployeeRepository
+    {
+        private readonly MVCAppContext _dbContext;
+        public EmployeeRepository(MVCAppContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public int Add(Employee employee)
+        {
+            _dbContext.Add(employee);
+            return _dbContext.SaveChanges();
+        }
+
+        public int Delete(Employee employee)
+        {
+            _dbContext.Remove(employee);
+            return _dbContext.SaveChanges();
+        }
+
+        public IEnumerable<Employee> GetAll()
+            => _dbContext.Employees.ToList();
+
+        public Employee GetById(int id)
+        {
+            //var Employee = _dbContext.Employees.Local.Where(d => d.Id == id).FirstOrDefault();
+            //if (Employee is null)
+            //	Employee = _dbContext.Employees.Where(d => d.Id == id).FirstOrDefault();
+            //return Employee;
+            return _dbContext.Employees.Find(id);
+        }
+
+        public int Update(Employee employee)
+        {
+            _dbContext.Update(employee);
+            return _dbContext.SaveChanges();
+        }
+    }
+}
