60d4e9957816575709ed316a7ee95cb8ff871c0f
abdosedroho <abdosedroho@gmail.com>
2024-04-05 05:23:33 +0200

    Send Email Function

--- Demo.PL/Helpers/EmailSettings.cs
+++ Demo.PL/Helpers/EmailSettings.cs
@@ -1,4 +1,6 @@
 ﻿using Demo.DAL.Models;
+using System.Net;
+using System.Net.Mail;
 
 namespace Demo.PL.Helpers
 {
@@ -6,7 +8,10 @@ namespace Demo.PL.Helpers
 	{
 		public static void SendEmail(Email email)
 		{
-
+			var client = new SmtpClient("smtp.gmail.com", 587);
+			client.EnableSsl = true;
+			client.Credentials = new NetworkCredential("abdosedroho@gmail.com", "ycak ebwh bcpr pfbl");
+			client.Send("abdosedroho@gmail.com", email.To, email.Subject, email.Body);
 		}
 	}
 }
