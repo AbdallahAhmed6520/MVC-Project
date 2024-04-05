8a939d8c2d6452b15d59980c938f9dfd312373dc
abdosedroho <abdosedroho@gmail.com>
2024-04-05 16:01:09 +0200

    Send Email Action

--- Demo.PL/Controllers/AccountController.cs
+++ Demo.PL/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using Demo.DAL.Models;
+using Demo.PL.Helpers;
 using Demo.PL.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -6,137 +7,149 @@ using System.Threading.Tasks;
 
 namespace Demo.PL.Controllers
 {
-	public class AccountController : Controller
-	{
-		private readonly UserManager<ApplicationUser> _userManager;
-		private readonly SignInManager<ApplicationUser> _signInManager;
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
 
-		public AccountController(
-			UserManager<ApplicationUser> userManager,
-			SignInManager<ApplicationUser> signInManager)
-		{
-			_userManager = userManager;
-			_signInManager = signInManager;
-		}
-		//Register
-		// BaseUrl/Account/Register
-		[HttpGet]
-		public IActionResult Register()
-		{
-			return View();
-		}
+        public AccountController(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        //Register
+        // BaseUrl/Account/Register
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
 
-		[HttpPost]
-		public async Task<IActionResult> Register(RegisterViewModel model)
-		{
-			if (ModelState.IsValid)
-			{
-				var user = new ApplicationUser
-				{
-					UserName = model.Email.Split('@')[0],
-					Email = model.Email,
-					FName = model.FName,
-					LName = model.LName,
-					IsAgree = model.IsAgree,
-				};
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = model.Email.Split('@')[0],
+                    Email = model.Email,
+                    FName = model.FName,
+                    LName = model.LName,
+                    IsAgree = model.IsAgree,
+                };
 
-				var Result = await _userManager.CreateAsync(user, model.Password);
+                var Result = await _userManager.CreateAsync(user, model.Password);
 
-				if (Result.Succeeded)
-				{
-					return RedirectToAction(nameof(Login));
-				}
-				else
-				{
-					foreach (var error in Result.Errors)
-					{
-						ModelState.AddModelError(string.Empty, error.Description);
-					}
-				}
-			}
-			return View(model);
-		}
+                if (Result.Succeeded)
+                {
+                    return RedirectToAction(nameof(Login));
+                }
+                else
+                {
+                    foreach (var error in Result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+            return View(model);
+        }
 
 
-		//LOGIN
-		[HttpGet]
-		public IActionResult Login()
-		{
-			return View();
-		}
+        //LOGIN
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
 
-		[HttpPost]
-		public async Task<IActionResult> Login(LoginViewModel model)
-		{
-			if (ModelState.IsValid)
-			{
-				var user = await _userManager.FindByEmailAsync(model.Email);
-				if (user != null)
-				{
-					var Result = await _userManager.CheckPasswordAsync(user, model.Password);
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(model.Email);
+                if (user != null)
+                {
+                    var Result = await _userManager.CheckPasswordAsync(user, model.Password);
 
-					if (Result)
-					{
-						//Login
-						var LoginResult = await _signInManager.PasswordSignInAsync(user, model.Password, model.RememberMe, false);
+                    if (Result)
+                    {
+                        //Login
+                        var LoginResult = await _signInManager.PasswordSignInAsync(user, model.Password, model.RememberMe, false);
 
-						if (LoginResult.Succeeded)
-						{
-							return RedirectToAction("Index", "Home");
-						}
-					}
-					else
-					{
-						ModelState.AddModelError(string.Empty, "Password is not Correct");
-					}
-				}
-				else
-				{
-					ModelState.AddModelError(string.Empty, "Email is not Exists");
-				}
-			}
-			return View(model);
-		}
+                        if (LoginResult.Succeeded)
+                        {
+                            return RedirectToAction("Index", "Home");
+                        }
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, "Password is not Correct");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Email is not Exists");
+                }
+            }
+            return View(model);
+        }
 
-		//SIGN OUT
-		public new async Task<IActionResult> SignOut()
-		{
-			await _signInManager.SignOutAsync();
-			return RedirectToAction(nameof(Login));
-		}
+        //SIGN OUT
+        public new async Task<IActionResult> SignOut()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction(nameof(Login));
+        }
 
-		//Forget Password
-		[HttpGet]
-		public IActionResult ForgetPassword()
-		{
-			return View();
-		}
+        //Forget Password
+        [HttpGet]
+        public IActionResult ForgetPassword()
+        {
+            return View();
+        }
 
-		[HttpPost]
-		public async Task<IActionResult> SendEmail(ForgetPasswordViewModel model)
-		{
-			if (ModelState.IsValid)
-			{
-				var user = await _userManager.FindByEmailAsync(model.Email);
-				if (user != null)
-				{
-					//Send Email
-					var email = new Email
-					{
-						Subject = "Reset Password",
-						To = user.Email,
-						Body = "ResetPasswordLink"
-					};
-				}
-				else
-				{
-					ModelState.AddModelError(string.Empty, "Email is not Exists");
-				}
-			}
-			else
-			{
-				return View("ForgetPassword", model);
-			}
-		}
-	}
+        [HttpPost]
+        public async Task<IActionResult> SendEmail(ForgetPasswordViewModel model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(model.Email);
+
+                var token = await _userManager.GeneratePasswordResetTokenAsync(user);
+                //Vaild For Only One Time For This User
+                //https://localhost:5001/Account/ResetPassword?email=abdosedroho@gmail.com?token=dbfsdjfbdjfndsjfbewmnvx
+                var ResetPasswordLink = Url.Action("ResetPassword", "Account", new { email = user.Email, Token = token }, Request.Scheme);
+
+
+                if (user != null)
+                {
+                    //Send Email
+                    var email = new Email
+                    {
+                        Subject = "Reset Password",
+                        To = user.Email,
+                        Body = ResetPasswordLink
+                    };
+                    EmailSettings.SendEmail(email);
+                    return RedirectToAction(nameof(CheckYourInbox));
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Email is not Exists");
+                }
+            }
+            return View("ForgetPassword", model);
+        }
+
+        public IActionResult CheckYourInbox()
+        {
+            return View();
+        }
+    }
 }
