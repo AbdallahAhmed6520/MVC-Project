3e789d8d075f56fd8e37892c62679433acacf02a
abdosedroho <abdosedroho@gmail.com>
2024-04-07 01:17:29 +0200

    Part 01 User Controller - Index

---
+++ Demo.PL/Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using Demo.DAL.Models;
+using Demo.PL.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo.PL.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> Index(string SearchValue)
+        {
+            if (string.IsNullOrEmpty(SearchValue))
+            {
+                var users = await _userManager.Users.Select(
+                    U => new UserViewModel()
+                    {
+                        Id = U.Id,
+                        FName = U.FName,
+                        LName = U.LName,
+                        Email = U.Email,
+                        Roles = _userManager.GetRolesAsync(U).Result
+                    }).ToListAsync();
+                return View(users);
+            }
+            else
+            {
+                var user = await _userManager.FindByEmailAsync(SearchValue);
+                var MappedUser = new UserViewModel()
+                {
+                    Id = user.Id,
+                    FName = user.FName,
+                    LName = user.LName,
+                    Email = user.Email,
+                    Roles = _userManager.GetRolesAsync(user).Result
+                };
+                return View(new List<UserViewModel> { MappedUser });
+            }
+        }
+    }
+}
